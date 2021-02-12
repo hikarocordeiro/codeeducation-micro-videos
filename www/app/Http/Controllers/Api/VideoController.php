41c343239b3a10965cca630d661e8fdfa304652c
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2021-02-12 20:04:09 -0300

    feat: resource videos

--- www/app/Http/Controllers/Api/VideoController.php
+++ www/app/Http/Controllers/Api/VideoController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Genre;
+use App\Models\Video;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class GenreController extends BasicCrudController
+class VideoController extends BasicCrudController
 {
-    private $rules = [
-        'name' => 'required|max:255',
-        'is_active' => 'boolean',
-        'categories_id' => 'required|array|exists:categories,id,deleted_at,NULL'
-    ];
+    private $rules;
+
+    public function __construct()
+    {
+        $this->rules = [
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'year_launched' => 'required|date_format:Y',
+            'opened' => 'boolean',
+            'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
+            'duration' => 'required|integer',
+            'categories_id' => 'required|array|exists:categories,id,deleted_at,NULL',
+            'genres_id' => 'required|array|exists:genres,id,deleted_at,NULL'
+        ];
+    }
 
     public function store(Request $request)
     {
@@ -32,9 +42,9 @@ class GenreController extends BasicCrudController
     public function update(Request $request, $id)
     {
         $obj = $this->findOrFail($id);
-        $validationData = $this->validate($request, $this->rulesUpdate());
         $self = $this;
-        DB::transaction(function () use ($self, $request, $obj, $validationData) {
+        $validationData = $this->validate($request, $this->rulesUpdate());
+        DB::transaction(function () use ($request, $validationData, $obj, $self) {
             $obj->update($validationData);
             $self->handleRelations($obj, $request);
         });
@@ -44,14 +54,15 @@ class GenreController extends BasicCrudController
     protected function handleRelations($video, Request $request)
     {
         $video->categories()->sync($request->get('categories_id'));
+        $video->genres()->sync($request->get('genres_id'));
     }
 
     protected function model()
     {
-        return Genre::class;
+        return Video::class;
     }
 
-    protected function rulesStore()
+    protected function RulesStore()
     {
         return $this->rules;
     }
---
+++ www/tests/Unit/Models/GenreUnitTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Genre;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Traits\Uuid;
+
+class GenreUnitTest extends TestCase
+{
+    private $genre;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->genre = new Genre();
+    }
+
+    public function testFillableAttribute()
+    {
+        $fillable = ['name', 'is_active'];
+
+        $this->assertEquals($fillable, $this->genre->getFillable());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [SoftDeletes::class, Uuid::class];
+        $genreTraits = array_keys(class_uses(Genre::class));
+
+        $this->assertEquals($traits, $genreTraits);
+    }
+
+    public function testCastsAttribute()
+    {
+        $casts = [
+            'id'=> 'string',
+            'is_active' => 'boolean'
+        ];
+
+        $this->assertEquals($casts, $this->genre->getCasts());
+    }
+
+    public function testIncrementingAttribute()
+    {
+        $this->assertFalse($this->genre->incrementing);
+    }
+
+    public function testDatesAttribute()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $genreDates = $this->genre->getDates();
+
+        $this->assertEqualsCanonicalizing(
+            $dates,
+            $genreDates
+        );
+
+        $this->assertCount(count($dates), $genreDates);
+    }
+}
